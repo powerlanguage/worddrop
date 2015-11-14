537c2eaaa3fee68f1cb2da7bb5d781090082d87e
powerlanguage <josh.wardle@gmail.com>
2015-11-14 12:21:53 -0800

    Basic box spawnder with rotation.

---
+++ Assets/Scripts/CreateBox.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateBox : MonoBehaviour {
+
+	public GameObject boxPrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("space")) {
+			float randomX = Random.Range(-5,5);
+			Quaternion randomRotation = Quaternion.Euler(0, 0, Random.Range(0,360));
+			GameObject newBox = (GameObject)Instantiate(boxPrefab, new Vector3(randomX, transform.position.y, transform.position.z), randomRotation);
+		}
+	}
+	
+}
