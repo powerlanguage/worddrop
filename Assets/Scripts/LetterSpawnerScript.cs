b9de103e7ce69b1e4779e881349c3c5bf819e7ef
powerlanguage <josh.wardle@gmail.com>
2015-11-14 17:40:10 -0800

    Letter colliders and random generator.

---
+++ Assets/Scripts/LetterSpawnerScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LetterSpawnerScript : MonoBehaviour {
+
+	public GameObject[] alphabet;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+//		if (Input.anyKey) {
+//			string letter = (string)Input.inputString;
+//			char character = letter[0];
+//			if(character >= 'a' && character <= 'z') {
+//				//Debug.Log (character);
+//				int index = char.ToUpper(character) - 'A';
+//				GameObject prefab = alphabetPrefabs[index];
+//				GameObject newGuy = (GameObject)Instantiate(prefab, new Vector3(transform.position.x, transform.position.y, transform.position.z), transform.rotation);
+//			}
+//
+//		}
+
+		if (Input.GetKeyDown ("space")) {
+			float randomX = Random.Range(-5,5);
+			Quaternion randomRotation = Quaternion.Euler(0, 0, Random.Range(0,360));
+			int randomIndex = (int)Random.Range(0, alphabet.Length);
+			GameObject letterPrefab = alphabet[randomIndex];
+			GameObject newBox = (GameObject)Instantiate(letterPrefab, new Vector3(randomX, transform.position.y, transform.position.z), randomRotation);
+		}
+	}
+	
+}
