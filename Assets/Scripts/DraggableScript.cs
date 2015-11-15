86f32d5a39aa000e765944acf94f594c0c0af900
powerlanguage <josh.wardle@gmail.com>
2015-11-14 18:25:36 -0800

    Add Torque & Force on instantiate.

---
+++ Assets/Scripts/DraggableScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DraggableScript : MonoBehaviour {
+
+	void OnMouseDrag()
+	{
+		float distance_to_screen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+		transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen ));
+	}
+}
--- Assets/Scripts/LetterSpawnerScript.cs
+++ Assets/Scripts/LetterSpawnerScript.cs
@@ -4,9 +4,13 @@ using System.Collections;
 public class LetterSpawnerScript : MonoBehaviour {
 
 	public GameObject[] alphabet;
+	public float xForceRange = 100;
+	public float maxTorque = 10;
+	GameObject newBox;
 
 	// Use this for initialization
 	void Start () {
+
 	}
 	
 	// Update is called once per frame
@@ -24,12 +28,28 @@ public class LetterSpawnerScript : MonoBehaviour {
 //
 //		}
 
+
 		if (Input.GetKeyDown ("space")) {
 			float randomX = Random.Range(-5,5);
+			Vector2 randomForce = new Vector2(Random.Range(-xForceRange,xForceRange), 0);
 			Quaternion randomRotation = Quaternion.Euler(0, 0, Random.Range(0,360));
 			int randomIndex = (int)Random.Range(0, alphabet.Length);
 			GameObject letterPrefab = alphabet[randomIndex];
-			GameObject newBox = (GameObject)Instantiate(letterPrefab, new Vector3(randomX, transform.position.y, transform.position.z), randomRotation);
+			newBox = (GameObject)Instantiate(letterPrefab, new Vector3(randomX, transform.position.y, transform.position.z), transform.rotation);
+			Rigidbody2D newBoxRigidbody = newBox.GetComponent<Rigidbody2D>();
+			newBoxRigidbody.AddForce(randomForce);
+			newBoxRigidbody.AddTorque(Random.Range(0,maxTorque));
+			Debug.Log(newBox);
+
+		}
+
+		if (Input.GetKeyDown ("return")) {
+			if(newBox != null) {
+				Vector2 forcey = new Vector2(0,600);
+				Debug.Log (newBox.transform.position.y);
+				newBox.GetComponent<Rigidbody2D>().AddForce(forcey);
+				Debug.Log (newBox.transform.position.y);
+			}
 		}
 	}
 	
