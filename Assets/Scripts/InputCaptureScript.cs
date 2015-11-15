54d31ff43a2d60ea2e1959581594a134188b7f19
powerlanguage <josh.wardle@gmail.com>
2015-11-14 22:09:51 -0800

    Input capturing.

---
+++ Assets/Scripts/InputCaptureScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputCaptureScript : MonoBehaviour {
+
+	public GameObject LetterSpawner;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.anyKeyDown) {
+			char inputChar = (char)Input.inputString[0];
+			LetterSpawnerScript spawner = LetterSpawner.GetComponent<LetterSpawnerScript>();
+			spawner.SpawnLetter(inputChar);
+		}
+	
+	}
+}
--- Assets/Scripts/LetterSpawnerScript.cs
+++ Assets/Scripts/LetterSpawnerScript.cs
@@ -16,41 +16,23 @@ public class LetterSpawnerScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-//		if (Input.anyKey) {
-//			string letter = (string)Input.inputString;
-//			char character = letter[0];
-//			if(character >= 'a' && character <= 'z') {
-//				//Debug.Log (character);
-//				int index = char.ToUpper(character) - 'A';
-//				GameObject prefab = alphabetPrefabs[index];
-//				GameObject newGuy = (GameObject)Instantiate(prefab, new Vector3(transform.position.x, transform.position.y, transform.position.z), transform.rotation);
-//			}
-//
-//		}
+	}
 
+	public void SpawnLetter(char optional = 'c'){
 
-		if (Input.GetKeyDown ("space")) {
-			float randomX = Random.Range(-5,5);
-			Vector2 randomForce = new Vector2(Random.Range(-xForceRange,xForceRange), 0);
-			Quaternion randomRotation = Quaternion.Euler(0, 0, Random.Range(0,360));
-			int randomIndex = (int)Random.Range(0, alphabet.Length);
-			GameObject letterPrefab = alphabet[randomIndex];
-			newBox = (GameObject)Instantiate(letterPrefab, new Vector3(randomX, transform.position.y, transform.position.z), transform.rotation);
-			Rigidbody2D newBoxRigidbody = newBox.GetComponent<Rigidbody2D>();
-			newBoxRigidbody.AddForce(randomForce);
-			newBoxRigidbody.AddTorque(Random.Range(0,maxTorque));
-			Debug.Log(newBox);
+		int letterIndex = optional - 97;
+		Debug.Log (letterIndex);
 
-		}
-
-		if (Input.GetKeyDown ("return")) {
-			if(newBox != null) {
-				Vector2 forcey = new Vector2(0,600);
-				Debug.Log (newBox.transform.position.y);
-				newBox.GetComponent<Rigidbody2D>().AddForce(forcey);
-				Debug.Log (newBox.transform.position.y);
-			}
-		}
+		float randomX = Random.Range(-5,5);
+		Vector2 randomForce = new Vector2(Random.Range(-xForceRange,xForceRange), 0);
+		Quaternion randomRotation = Quaternion.Euler(0, 0, Random.Range(0,360));
+		int randomIndex = (int)Random.Range(0, alphabet.Length);
+		GameObject letterPrefab = alphabet[letterIndex];
+		newBox = (GameObject)Instantiate(letterPrefab, new Vector3(randomX, transform.position.y, transform.position.z), transform.rotation);
+		Rigidbody2D newBoxRigidbody = newBox.GetComponent<Rigidbody2D>();
+		newBoxRigidbody.AddForce(randomForce);
+		newBoxRigidbody.AddTorque(Random.Range(0,maxTorque));
+		Debug.Log(newBox);
 	}
 	
 }
